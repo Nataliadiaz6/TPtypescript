3b7e1a51deb1a62b69e3635b072bb2bc393f1f25
Nataliadiaz6 <nataliadiazg6@gmail.com>
2026-05-07 01:05:36 -0300

    Add files via upload

---
+++ productos.ts
@@ -0,0 +1,59 @@
+export const categorias = [
+  "Hamburguesas",
+  "Pizzas",
+  "Lomitos",
+  "Papas Fritas",
+  "Milanesas",
+  "Bebidas"
+];
+
+export const productos = [
+  {
+    id: 1,
+    nombre: "Hamburguesa Simple",
+    descripcion: "Un medallon de carne vacuna de primera calidad",
+    precio: 14000,
+    imagen: "/assets/hamburguesa-triple.jpg",
+    categoria: "Hamburguesas"
+  },
+  {
+    id: 2,
+    nombre: "Pizza Muzzarella",
+    descripcion: "Salsa casera y abundante queso",
+    precio: 18000,
+    imagen: "/assets/pizza-muzzarella.jpg",
+    categoria: "Pizzas"
+  },
+  {
+    id: 3,
+    nombre: "Lomito Completo",
+    descripcion: "Bife de lomo, huevo, jamón, queso y lechuga",
+    precio: 15000,
+    imagen: "/assets/lomito-completo.jpg",
+    categoria: "Lomitos"
+  },
+  {
+    id: 4,
+    nombre: "Papas Grandes",
+    descripcion: "Papas fritas con sal de mar",
+    precio: 8500,
+    imagen: "/assets/papas-grandes.jpg",
+    categoria: "Papas Fritas"
+  },
+  {
+    id: 5,
+    nombre: "Milanesa a Caballo",
+    descripcion: "Milanesas de carne con huevo frito encima",
+    precio: 13000,
+    imagen: "/assets/milanesa-caballo.jpg",
+    categoria: "Milanesas"
+  },
+  {
+    id: 6,
+    nombre: "Gaseosa Cola",
+    descripcion: "500ml bien fría",
+    precio: 3500,
+    imagen: "/assets/gaseosa-cola.jpg",
+    categoria: "Bebidas"
+  }
+];
