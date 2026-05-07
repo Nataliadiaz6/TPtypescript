0b4c1e9a3a7b3f9ba09917e0a47f9e667bb4e146
Nataliadiaz6 <nataliadiazg6@gmail.com>
2026-05-07 00:57:23 -0300

    Add files via upload

--- registro.ts
+++ registro.ts
@@ -1,9 +1,41 @@
+import type { IUser } from "../../../types/IUser";
+import { Rol } from "../../../types/rol";
 
-import { logout } from "../../../utils/auth";
+const form = document.querySelector<HTMLFormElement>("#registroForm");
 
-const buttonLogout = document.getElementById(
-  "logoutButton"
-) as HTMLButtonElement;
-buttonLogout?.addEventListener("click", () => {
-  logout();
-});
+if (form) {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const data = new FormData(form);
+
+    const email = data.get("email") as string;
+    const password = data.get("password") as string;
+
+    if (!email || !password) {
+      alert("Completa todos los campos");
+      return;
+    }
+
+    const users: IUser[] = JSON.parse(localStorage.getItem("users") || "[]");
+
+    const exists = users.some((u) => u.email === email);
+
+    if (exists) {
+      alert("El usuario ya existe");
+      return;
+    }
+
+    const newUser: IUser = {
+      email,
+      password,
+      rol: Rol.CLIENT,
+    };
+
+    users.push(newUser);
+    localStorage.setItem("users", JSON.stringify(users));
+
+    alert("Usuario registrado correctamente");
+    window.location.href = "/src/pages/auth/login/login.html";
+  });
+}
