6d880abc41b680d1fc6ba711e3ce653d32d7e399
Nataliadiaz6 <nataliadiazg6@gmail.com>
2026-05-07 01:00:08 -0300

    Add files via upload

--- auth.ts
+++ auth.ts
@@ -1,39 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/src/style.css"> <!-- unimos a CSS -->
-    <title>Document</title>
-</head>
-
-<body>
-    <h1>Registro</h1>
-
-    <!-- Formulario que usa tu registro.ts -->
-    <form id="registroForm">
-        
-        <!-- Input email -->
-        <input 
-            type="email" 
-            name="email" 
-            placeholder="Email" 
-        />
-
-        <!-- Input password -->
-        <input 
-            type="password" 
-            name="password" 
-            placeholder="Contraseña" 
-        />
-
-        <!-- Botón enviar -->
-        <button type="submit">Registrarse</button>
-
-    </form>
-    <script type="module" src="/src/main.ts"></script>
-    <script type="module" src="./registro.ts"></script>
-</body>
-</html>
-
+// la seguridad la deja,os solamente el el main.ts
+// funcion para cerrar sesion
+export function logout() {
+// borramos usuario loguado
+  localStorage.removeItem("userData");
+//redirigimos al login
+  window.location.href = "/src/pages/auth/login/login.html";
+}
--- localStorage.ts
+++ localStorage.ts
@@ -1,41 +1,12 @@
-import type { IUser } from "../../../types/IUser";
-import { Rol } from "../../../types/rol";
+import type { IUser } from "../types/IUser";
 
-const form = document.querySelector<HTMLFormElement>("#registroForm");
-
-if (form) {
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    const data = new FormData(form);
-
-    const email = data.get("email") as string;
-    const password = data.get("password") as string;
-
-    if (!email || !password) {
-      alert("Completa todos los campos");
-      return;
-    }
-
-    const users: IUser[] = JSON.parse(localStorage.getItem("users") || "[]");
-
-    const exists = users.some((u) => u.email === email);
-
-    if (exists) {
-      alert("El usuario ya existe");
-      return;
-    }
-
-    const newUser: IUser = {
-      email,
-      password,
-      rol: Rol.CLIENT,
-    };
-
-    users.push(newUser);
-    localStorage.setItem("users", JSON.stringify(users));
-
-    alert("Usuario registrado correctamente");
-    window.location.href = "/src/pages/auth/login/login.html";
-  });
-}
+export const saveUser = (user: IUser) => {
+  const parseUser = JSON.stringify(user);
+  localStorage.setItem("userData", parseUser);
+};
+export const getUser = () => {
+  return localStorage.getItem("userData");
+};
+export const removeUser = () => {
+  localStorage.removeItem("userData");
+};
